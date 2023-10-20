6d19e807b1518843f17069e75fae7413195f4b56
alissonsteen <alisson.a.m88@gmail.com>
2023-10-19 21:54:00 -0300

    main

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Appbar , PaperProvider } from 'react-native-paper';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import {expo} from './app.json'
+
 
 export default function App() {
+
+  const MyComponent = () => (
+    <Appbar.Header>
+      <Appbar.BackAction onPress={() => {}} />
+      <Appbar.Content title="Title" />
+      <Appbar.Action icon="calendar" onPress={() => {}} />
+      <Appbar.Action icon="magnify" onPress={() => {}} />
+    </Appbar.Header>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text> Teste </Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <MyComponent></MyComponent>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent(expo.name, () => App)
