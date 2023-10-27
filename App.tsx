6cd509226eb3aa3ea0458e1e01497afd6af0c8c4
alexandrekawan7 <alexandrekawan2003@gmail.com>
2023-10-26 21:09:59 -0300

    Start login page

--- App.tsx
+++ App.tsx
@@ -1,26 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Appbar , PaperProvider } from 'react-native-paper';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import {expo} from './app.json'
+import { AppRegistry, StyleSheet, Text, View, SafeAreaView, TextInput, Button, Pressable } from 'react-native';
+import { expo } from './app.json'
 
 
 export default function App() {
-
-  const MyComponent = () => (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={() => {}} />
-      <Appbar.Content title="Teste" />
-      <Appbar.Action icon="calendar" onPress={() => {}} />
-      <Appbar.Action icon="magnify" onPress={() => {}} />
-    </Appbar.Header>
-  );
-
   return (
-    <PaperProvider>
-      <MyComponent></MyComponent>
-    </PaperProvider>
-  );
+    <SafeAreaView style={styles.form}>
+      <Text style={styles.heading}>Login</Text>
+
+      <TextInput style={styles.input} placeholder='E-Mail'></TextInput>
+      <TextInput style={styles.input} placeholder='Senha'></TextInput>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.button.innerText}>Oi</Text>
+      </Pressable>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 10
+  },
+  heading: {
+    fontSize: 55,
+    fontWeight: 'bold',
+    marginBottom: '5%'
+  },
+  input: {
+    borderColor: '#000000',
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '70%',
+    height: '5%',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#000000',
+    width: '70%',
+    height: '5%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    innerText: {
+      color: '#ffffff'
+    }
+  }
+})
 
 AppRegistry.registerComponent(expo.name, () => App)
